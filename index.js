a10860027906b3cb37dbc2efd298041f2666e718
Matthew Voss <voss.matthew@gmail.com>
2014-05-05 12:15:01 -0500

    revert

--- index.js
+++ index.js
@@ -1181,11 +1181,10 @@ Registrants.prototype.searchAttendees = function(fields, search, page, limit, ex
               "LEFT JOIN biller ON (exhibitorAttendees.userId = biller.userID AND exhibitorAttendees.eventId = biller.eventId) "+
               "WHERE biller.status != -1 AND (";
         fields.forEach(function(field, index) {
-          var exField = underscore.findWhere(exhFields, {class: field});
           if (index > 0) {
               sql += " OR ";
           }
-          sql += "exhibitorAttendees."+exhField.id+" LIKE ?";
+          sql += "exhibitorAttendees."+field+" LIKE ?";
           vars.push("%"+search+"%");
         });
         sql += "))";
